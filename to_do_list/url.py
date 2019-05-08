4e725fd3871ba907f235e53154e39add4b50e6e6
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-05-08 16:30:43 +0600

    working on editing the task function

--- to_do_list/url.py
+++ to_do_list/url.py
@@ -7,4 +7,5 @@ urlpatterns = [
      path('delete/<list_id>', views.delete, name='delete'),
      path('done/<list_id>', views.done, name='done'),
      path('pending/<list_id>', views.pending, name='pending'),
+     path('edit/<list_id>', views.edit, name='edit'),
 ]
